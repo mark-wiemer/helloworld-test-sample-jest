d45d5644bdc8b6f8bb2152143d2f5d787d2e537e
Mark Wiemer <markwiemer@outlook.com>
2021-02-17 00:03:21 -0800

    Remove unused import

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,13 @@
-import * as assert from 'assert';
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
 
 describe('Extension Test Suite', () => {
-	console.log('Running tests');
-	vscode.window.showInformationMessage('Start all tests.');
+    console.log('Running tests');
+    vscode.window.showInformationMessage('Start all tests.');
 
-	it('Sample test', () => {
-		expect(1 + 1).toBe(2);
-	});
+    it('Sample test', () => {
+        expect(1 + 1).toBe(2);
+    });
 });
